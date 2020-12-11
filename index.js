d6d8f3d31d1538f4224d43474db10d027c9c5855
SOURAV ROY <souravroy.info13@gmail.com>
2020-12-11 23:45:02 +0600

    Need More Children cluster.fork() ✔💖🚀

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 let cluster = require("cluster");
 
 //Is the file being executed in master mode ??
@@ -5,28 +6,38 @@ if (cluster.isMaster) {
 
     //Cause index.js to be executed **again** child mode
     cluster.fork();
-    
+
     cluster.fork();
     cluster.fork();
     cluster.fork();
+    cluster.fork();
+    cluster.fork();
+
+    // cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
 } else {
 
     //In a child Im going to act like a server && do not nothing else
     let express = require("express");
+    let crypto = require('crypto');
     let App = express();
 
-    function doWork(duration) {
-        let time = Date.now();
-        while (Date.now() - time < duration) {
-        }
-    }
+    // function doWork(duration) {
+    //     let time = Date.now();
+    //     while (Date.now() - time < duration) {
+    //     }
+    // }
 
     App.get('/', function (req, res) {
 
         //Single Thread Duration ************************
-        doWork(4000);
+        // doWork(4000);
 
-        res.send('Hello Sourav');
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            res.send('Hello Sourav');
+        })
     })
     App.get("/first", function (req, res) {
         res.send('How fast is this !!');
