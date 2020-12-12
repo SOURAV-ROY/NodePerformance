4fd7f7d15683589ef77973a97df279786eaa89ab
SOURAV ROY <souravroy.info13@gmail.com>
2020-12-12 17:29:46 +0600

    pm2 start in port 2020 is DONE

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-process.env.UV_THREADPOOL_SIZE = 1;
-let cluster = require("cluster");
+// process.env.UV_THREADPOOL_SIZE = 1;
+// let cluster = require("cluster");
 
 //Is the file being executed in master mode ??
-if (cluster.isMaster) {
+// if (cluster.isMaster) {
 
-    //Cause index.js to be executed **again** child mode
-    cluster.fork();
+//Cause index.js to be executed **again** child mode
+// cluster.fork();
+//
+// cluster.fork();
+// cluster.fork();
+// cluster.fork();
+// cluster.fork();
+// cluster.fork();
 
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
+// cluster.fork();
+// cluster.fork();
+// cluster.fork();
+// cluster.fork();
+// } else {
 
-    // cluster.fork();
-    // cluster.fork();
-    // cluster.fork();
-    // cluster.fork();
-} else {
+//In a child Im going to act like a server && do not nothing else
+let express = require("express");
+let crypto = require('crypto');
+let App = express();
 
-    //In a child Im going to act like a server && do not nothing else
-    let express = require("express");
-    let crypto = require('crypto');
-    let App = express();
+// function doWork(duration) {
+//     let time = Date.now();
+//     while (Date.now() - time < duration) {
+//     }
+// }
 
-    // function doWork(duration) {
-    //     let time = Date.now();
-    //     while (Date.now() - time < duration) {
-    //     }
-    // }
+App.get('/', function (req, res) {
 
-    App.get('/', function (req, res) {
+    //Single Thread Duration ************************
+    // doWork(4000);
 
-        //Single Thread Duration ************************
-        // doWork(4000);
-
-        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-            res.send('Hello Sourav');
-        })
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        res.send('Hello Sourav 2020');
     })
-    App.get("/first", function (req, res) {
-        res.send('How fast is this !!');
-    })
+})
+App.get("/first", function (req, res) {
+    res.send('How fast is this !!');
+})
 
-    let Port = 2020;
-    App.listen(Port, function () {
-        console.log(`server Run ${Port}`);
-    })
-}
+let Port = 2021;
+App.listen(Port, function () {
+    console.log(`server Run ${Port}`);
+})
+// }
